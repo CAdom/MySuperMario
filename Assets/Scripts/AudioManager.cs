cecf52869d7decb4ec09a505f16c85fe8f780a9d
亚 锁 <1476769384@qq.com>
2019-11-21 12:50:39 +0800

    mario demo

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    public static AudioManager Instance;
+
+    //获取声音播放组件 
+    private AudioSource player;
+
+	void Start () {
+        Instance = this;            //创建静态自身对象
+        //获取音频播放组件
+        player = GetComponent<AudioSource>();
+	}
+    //播放音效
+    public void PlaySound(string name)
+    {
+        //加载的音效
+       AudioClip clip =Resources.Load<AudioClip>(name);
+        //播放
+        player.PlayOneShot(clip);
+    }
+    //停止播放声音
+    public void StopSound()
+    {
+        player.Stop();
+    }
+}
