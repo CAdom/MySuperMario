45fc60692c333615d0aa94c78e76af67611b4d9b
CAdom <45619532+CAdom@users.noreply.github.com>
2019-12-02 22:26:45 +0800

    我好难

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Login : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour {
+
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
 
     public InputField AccountInput;//账号输入
     public InputField PasswordInput;//密码输入
     public Text Mistake;//账号及密码输入错误提示
     public Text Register;//登录成功提示
 
+    public string MySuperMario { get; private set; }
+
     public void OnButton()//Button点击事件
     {
         string AccountNumber = AccountInput.text;//获取输入账号
         string PassWordNumber = PasswordInput.text;//获取输入密码
-        if (AccountNumber == "suoya" && PassWordNumber == "123321")//判断是否输入正确
+        if (AccountNumber == "2017301110107" && PassWordNumber == "123")//判断是否输入正确
         {
             Register.gameObject.SetActive(true);//登录效果提示
             StartCoroutine(Disappear());//调用协程
-            SceneManager.LoadScene(1);//跳转别的场景
+            SceneManager.LoadScene(MySuperMario);//跳转别的场景
         }
         else
         {
@@ -33,4 +40,15 @@ public class Login : MonoBehaviour {
         Mistake.gameObject.SetActive(false);//提示错误效果消失
         Register.gameObject.SetActive(false);//提示登录成功效果消失
     }
+    /*
+        // Use this for initialization
+        void Start () {
+             DontDestroyOnLoad(this);
+        }
+
+        // Update is called once per frame
+        void Update () {
+
+        }
+    */
 }
